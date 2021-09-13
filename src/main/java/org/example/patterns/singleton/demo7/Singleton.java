b125aa3a43d0ccea22058e1c337f0f19c176d6fe
Zijie Zhao <Zijie.Zhao@cicc.com.cn>
2021-09-13 17:14:52 +0800

    序列化破坏单例模式的解决方案

--- src/main/java/org/example/patterns/singleton/demo7/Singleton.java
+++ src/main/java/org/example/patterns/singleton/demo7/Singleton.java
@@ -6,10 +6,18 @@ public class Singleton implements Serializable {
 
     private Singleton() {}
 
-    public static Singleton instance;
+    private static class SingletonHolder {
+        // 在内部类中声明并初始化外部类的对象
+        private static final Singleton INSTANCE = new Singleton();
+    }
 
+    //提供公共访问方式
     public static Singleton getInstance() {
-        instance = new Singleton();
-        return instance;
+        return SingletonHolder.INSTANCE;
+    }
+
+    //当进行反序列化时,会自动调用该方法,将该方法的返回值直接返回
+    public Object readResolve() {
+        return SingletonHolder.INSTANCE;
     }
 }
