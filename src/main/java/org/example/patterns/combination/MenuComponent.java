fcedc93560d0e94a55ed1c448d4a0149dbfaaee6
Zijie Zhao <Zijie.Zhao@cicc.com.cn>
2021-09-27 17:26:07 +0800

    组合模式案例实现

---
+++ src/main/java/org/example/patterns/combination/MenuComponent.java
@@ -0,0 +1,34 @@
+package org.example.patterns.combination;
+
+/**
+ * 抽象根节点
+ */
+public abstract class MenuComponent {
+    //菜单组件的名称
+    protected String name;
+    //菜单组件的层级
+    protected int level;
+
+    //添加子菜单
+    public void add(MenuComponent menuComponent) {
+        throw new UnsupportedOperationException();
+    }
+
+    //移除子菜单
+    public void remove(MenuComponent menuComponent) {
+        throw new UnsupportedOperationException();
+    }
+
+    //获取指定的子菜单
+    public MenuComponent getChild(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    //获取菜单或者菜单项的名称
+    public String getName() {
+        return name;
+    }
+
+    //打印菜单名称(包含子菜单和子菜单项)
+    public abstract void print();
+}
---
+++ src/main/java/org/example/patterns/combination/MenuItem.java
@@ -0,0 +1,21 @@
+package org.example.patterns.combination;
+
+/**
+ * 叶子节点
+ */
+public class MenuItem extends MenuComponent{
+
+    public MenuItem(String name,int level) {
+        this.name = name;
+        this.level = level;
+    }
+
+    @Override
+    public void print() {
+        //打印菜单项的名称
+        for (int i = 0; i < level;i++) {
+            System.out.print("--");
+        }
+        System.out.println(name);
+    }
+}
